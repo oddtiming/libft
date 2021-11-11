96467014411133944f48af93a4490ac6384d2dce
Ismael Yahyaoui Racine <iyahoui-@student.42quebec.com>
2021-11-11 16:26:31 -0500

    corrected get_magnitude

--- ft_get_magnitude.c
+++ ft_get_magnitude.c
@@ -6,7 +6,7 @@
 /*   By: iyahoui- <iyahoui-@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 13:35:14 by iyahoui-          #+#    #+#             */
-/*   Updated: 2021/11/11 13:37:40 by iyahoui-         ###   ########.fr       */
+/*   Updated: 2021/11/11 16:09:03 by iyahoui-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,24 +17,24 @@ int	get_magnitude(int n)
 {
 	int	magnitude;
 
-	magnitude = 0;
+	magnitude = 1;
 	while (n)
 	{
 		magnitude *= 10;
 		n /= 10;
 	}
-	return (magnitude + 1);
+	return (magnitude / 10);
 }
 
 int	get_magnitude_unsigned(unsigned int n)
 {
 	int	magnitude;
 
-	magnitude = 0;
-	while (n)
+	magnitude = 1;
+	while (n >= 10)
 	{
 		magnitude *= 10;
 		n /= 10;
 	}
-	return (magnitude + 1);
+	return (magnitude);
 }
